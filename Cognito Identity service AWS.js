4ddf28066268ada27ff8188b2915ad3048160a5a
BitInvader <maier.kvn@gmail.com>
2023-05-05 12:28:19 +0000

    Update Cognito Identity service AWS.js

--- Cognito Identity service AWS.js
+++ Cognito Identity service AWS.js
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AWS Identity Cognito</title>
   <script src="https://sdk.amazonaws.com/js/aws-sdk-2.1.50.min.js"></script>
 </head>
 
